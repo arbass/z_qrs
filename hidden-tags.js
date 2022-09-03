8e51cd2903df796e1bca61774c3582060b7b5873
arbass <13ar.bass@gmail.com>
2022-09-03 18:19:15 +0300

    updated: max of visible cards works with el attribute's value

--- hidden-tags.js
+++ hidden-tags.js
@@ -1,4 +1,4 @@
-//@collapse
+////@collapse
 //|webflow| ПОДГОТОВКА К РАБОТЕ С ФУНКЦИЕЙ
 //|webflow| 1. Найдите один экземляр тэга (лучше collection list item) и дайте ему аттрибут hidden-tags-counter="tag", ничего страшного, 
 //|webflow| если он ещё находится не в карточке, а на пример в коллекции, которая нужна для finsweet с аттрибутом fs-cmsnest-collection="---"
@@ -20,6 +20,7 @@
 //⭐️ Entry point[java script] Ищем все CLW, в которых будут мутации
 let ht__allMutatuinClw = document.querySelectorAll('[hidden-tags-counter="mutation-observer"]');
 let ht__mutationTimer;
+let ht__status = false;
 ht__allMutatuinClw.forEach(el => {
     // Выбираем целевой элемент
     let ht__target = el;
@@ -38,34 +39,42 @@ ht__allMutatuinClw.forEach(el => {
 
 //[java script] Найдём все родительские элементы с тэгом hidden-tags-counter="tags-wrapper" 
 function ht__showTags() {
-    console.log('запустили по мутации');
-    // let ht__allTagsParents = document.querySelectorAll('[hidden-tags-counter="main-parent"]');
-    //     ht__allTagsParents.forEach(el => {
-    //         let ht__countOfVisibleTags = el.querySelector('[hidden-tags-counter-max]').getAttribute('hidden-tags-counter-max');
-    //         //переводим ht__countOfVisibleTags в число
-    //         ht__countOfVisibleTags = Number(ht__countOfVisibleTags);
-    //         //теперь для всех тэгов, кроме первых ht__countOfVisibleTags, добавим класс hide
-    //         let allCurrentTags = el.querySelectorAll('[hidden-tags-counter="tag"]');
-    //         allCurrentTags.forEach(tag => {
-    //             tag.classList.add('hide');
-    //         });
-    //         let ht__zero = 0;
-    //         // while (ht__zero < ht__countOfVisibleTags) {
-    //         //     allCurrentTags[ht__zero].classList.remove('hide');
-    //         //     ht__zero++;
-    //         // }
-    //         //перед началом работы скрипта убедимся, что у обёртки тэгов всё ещё нет сласса opacity-full
-    //         let currentTagWrapper = el.querySelector('[hidden-tags-counter="tags-wrapper"]');
-    //         if (currentTagWrapper.classList.contains('opacity-full') == false) {
-                
-                
-                
-                
-                
-                
-                
-    //             //самое последние действие — выводи обёртку с тэгами из опасити
-    //             currentTagWrapper.classList.add('opacity-full');
-    //         }
-    // });
+    clearTimeout(ht__mutationTimer);
+    if (ht__status == false) {
+        ht__mutationTimer = setTimeout(() => {
+            ht__status = true;
+            let ht__allTagsParents = document.querySelectorAll('[hidden-tags-counter="main-parent"]');
+                ht__allTagsParents.forEach(el => {                    
+                    let ht__interator = 0;
+                    let ht__countOfVisibleTags = el.querySelector('[hidden-tags-counter-max]').getAttribute('hidden-tags-counter-max');
+                    //переводим ht__countOfVisibleTags в число
+                    ht__countOfVisibleTags = Number(ht__countOfVisibleTags);
+                    //теперь для всех тэгов, кроме первых ht__countOfVisibleTags, добавим класс hide
+                    let allCurrentTags = el.querySelectorAll('[hidden-tags-counter="tag"]');
+                    allCurrentTags.forEach(tag => {
+                        tag.classList.add('hide');
+                    });
+                    while (ht__interator < ht__countOfVisibleTags) {
+                        // console.log('итератор равен = ' + ht__interator);
+                        // console.log('поэтоу раскрываем этот элемент –> ' + allCurrentTags[ht__interator]);
+                        if (allCurrentTags[ht__interator] != undefined) {
+                            allCurrentTags[ht__interator].classList.remove('hide');
+                        }
+                        ht__interator++;
+                    }
+                    //перед началом работы скрипта убедимся, что у обёртки тэгов всё ещё нет сласса opacity-full
+                    let currentTagWrapper = el.querySelector('[hidden-tags-counter="tags-wrapper"]');
+                    if (currentTagWrapper.classList.contains('opacity-full') == false) {
+                        //самое последние действие — выводи обёртку с тэгами из опасити
+                        currentTagWrapper.classList.add('opacity-full');
+                    }
+
+                    // console.log('конец карточки––––––––––––––––––––––––');
+            });
+
+            setTimeout(() => {
+                ht__status = false;
+            }, 350);
+        }, 300);
+    }
 };
