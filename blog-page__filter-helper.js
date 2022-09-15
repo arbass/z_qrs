0e0c5f16b3e323c1c53278164f8296c084c26cf1
arbass <13ar.bass@gmail.com>
2022-09-15 17:16:32 +0300

    updated: selected topics displays in dropdown

--- blog-page__filter-helper.js
+++ blog-page__filter-helper.js
@@ -19,7 +19,6 @@ function activeState__syncCategories () {
 function activeState__syncTopic () {
     fhTrue__mirror__allTopicButtonsPc.forEach((fhTrue__topicPc, fhTrue__topicPcId) => {
         if (fhTrue__topicPc.classList.contains('is-active-topic')) {
-            console.log(fhTrue__topicPc);
             fhFalse__mirror__allTopicButtonsPc[fhTrue__topicPcId].classList.add('is-active-topic');
             fhFalse__mirror__allTopicButtonsMobile[fhTrue__topicPcId].classList.add('is-active-topic');
         } else {
@@ -29,10 +28,29 @@ function activeState__syncTopic () {
     });
 }
 
+function activeState__syncTopicByComa () {
+    let fh__listOfActiveTopics = new Array();
+    fhTrue__mirror__allTopicButtonsPc.forEach(fhTrue__topicPc => {
+        if (fhTrue__topicPc.classList.contains('is-active-topic')) {
+            fh__listOfActiveTopics.push(fhTrue__topicPc.querySelector('.blog-articles-filter-and-search_radio-button-text').textContent + ', ');
+        }
+    });
+    if (fh__listOfActiveTopics.length > 0) {
+        let currentText = '';
+        fh__listOfActiveTopics.forEach(topic => {
+            currentText = currentText + topic;
+        });
+        document.querySelector('.filter-and-search-mob_category-dropdown-text.is-topic').textContent = currentText.slice(0, -2);
+    } else {
+        document.querySelector('.filter-and-search-mob_category-dropdown-text.is-topic').textContent = 'Show all';
+    }
+}
+
 function activeState__sync () {
     setTimeout(() => {
         activeState__syncCategories();
         activeState__syncTopic();
+        activeState__syncTopicByComa();
     }, 100);
 }
 
