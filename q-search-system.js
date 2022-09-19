02736424984032736529340dd05c094ee2d67030
arbass <13ar.bass@gmail.com>
2022-09-19 12:11:32 +0300

    created: base for filter

---
+++ q-search-system.js
@@ -0,0 +1,33 @@
+let ss__trueInput = document.querySelector('[ss_input="true"]');
+let ss__allNamesElements = document.querySelectorAll('.form_search-nav-cl-i_name');
+let ss__allNames = new Array();
+console.log(ss__allNames);
+
+ss__allNamesElements.forEach(ss__name => {
+    ss__allNames.push(ss__name.textContent);
+});
+
+
+ss__trueInput.addEventListener('keydown', function() {
+    ss__inputJob();
+});
+
+
+ss__trueInput.addEventListener('keyup', function() {
+    ss__inputJob();
+});
+
+
+ss__trueInput.addEventListener('change', function() {
+    ss__inputJob();
+});
+
+function ss__inputJob () {
+    keyword = ss__trueInput.value.toLowerCase();
+    filtered_ss__allNames = ss__allNames.filter(function(name){
+          name = name.toLowerCase();
+         return name.indexOf(keyword) > -1; 
+    });
+  
+    console.log(filtered_ss__allNames);
+}
