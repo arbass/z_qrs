2d91812bef95655ed135b4fe28633140d202c249
arbass <13ar.bass@gmail.com>
2022-09-25 00:31:20 +0300

    testimonials masonry is ready!

--- ak-masonry.js
+++ ak-masonry.js
@@ -1,25 +1,33 @@
 let ak__allGrid = document.querySelectorAll('.home-accelerating-careers_grid');
-let ak__secondCol = document.querySelector('.home-accelerating-careers_grid-col.is-2nd');
+let ak__allFridItems_1 = ak__allGrid[0].querySelectorAll('.cl-i__home-accelerating-careers');
+
+if (ak__allFridItems_1.length < 6) {
+    document.querySelector('.home-accelerating-careers_button-wrapper').remove();
+    document.querySelector('.button-more.is-testimonials').remove();
+} 
 
 ak__allGrid.forEach(ak__grid => {
-    let ak__allGridItems = ak__grid.querySelectorAll('.home-accelerating-careers_grid-item');
-    let ak__allGridItemsLength = ak__allGridItems.length;
+    let ak__currentItems = ak__grid.querySelectorAll('.cl-i__home-accelerating-careers');
+    let ak__currentItemsLength = ak__currentItems.length;
+    let ak__currentAppendWaiter = ak__grid.querySelector('.home-accelerating-careers_grid-col.is-2nd');
 
-    if(ak__allGridItemsLength % 2 == 0) {
-        ak__allGridItemsLength = ak__allGridItemsLength / 2;
-        ak__allGridItems.forEach((ak__gridItem, ak__gridItemId) => {
-            if (ak__gridItemId == ak__allGridItemsLength) {
-                ak__secondCol.append(ak__gridItem.parentElement);
-                ak__allGridItemsLength++;
+    if(ak__currentItemsLength % 2 == 0) {
+        ak__currentItemsLength = ak__currentItemsLength / 2;
+        ak__currentItems.forEach((ak__items,ak__itemsId) => {
+            if (ak__currentItemsLength == ak__itemsId) {
+                ak__currentAppendWaiter.append(ak__items);
+                console.log(ak__items);
+                ak__currentItemsLength++;
             }
         });
     }
+
     else {
-        ak__allGridItemsLength = (ak__allGridItemsLength / 2) + 0.5;
-        ak__allGridItems.forEach((ak__gridItem, ak__gridItemId) => {
-            if (ak__gridItemId == ak__allGridItemsLength) {
-                ak__secondCol.append(ak__gridItem.parentElement);
-                ak__allGridItemsLength++;
+        ak__currentItemsLength = (ak__currentItemsLength / 2) + 0.5;
+        ak__currentItems.forEach((ak__items,ak__itemsId) => {
+            if (ak__currentItemsLength == ak__itemsId) {
+                ak__currentAppendWaiter.append(ak__items);
+                ak__currentItemsLength++;
             }
         });
     }
