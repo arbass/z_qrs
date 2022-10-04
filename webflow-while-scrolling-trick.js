cb92341a65011ae205368e5a433ef2b9206a1863
arbass <13ar.bass@gmail.com>
2022-10-04 14:41:17 +0300

    updated: scrolling-trick

--- webflow-while-scrolling-trick.js
+++ webflow-while-scrolling-trick.js
@@ -1,14 +1,16 @@
 const wwst__opacityElements__array = document.querySelectorAll('[wwst-opacity="true"]');
 
 function wwst__opacityMagic () {
-    wwst__opacityElements__array.forEach(wwst__opacityElements__item => {
-        //if wwst__opacityElements__item opacity is 0
-        if (wwst__opacityElements__item.style.opacity == 0) {
-            wwst__opacityElements__item.classList.add('hide');
-        } else {
-            wwst__opacityElements__item.classList.remove('hide');
-        }
-    });
+    if (window.innerWidth > 767) {
+        wwst__opacityElements__array.forEach(wwst__opacityElements__item => {
+            //if wwst__opacityElements__item opacity is 0
+            if (wwst__opacityElements__item.style.opacity == 0) {
+                wwst__opacityElements__item.classList.add('hide');
+            } else {
+                wwst__opacityElements__item.classList.remove('hide');
+            }
+        });
+    }
 }
 
 addEventListener('scroll', (event) => {
