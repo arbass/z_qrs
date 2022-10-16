3c93ce470d50d7e339c2b7205c6d042bb12bb138
arbass <13ar.bass@gmail.com>
2022-10-16 09:57:27 +0300

    updated: s-o-t.js

--- slider-opacity-trick.js
+++ slider-opacity-trick.js
@@ -1,46 +1,55 @@
+function lpSwiperMagic () {
+    setTimeout(function () {
+        let lpSlider = document.querySelector('.swiper.lp-slider');
+        let slider__lp1 = lpSlider.querySelector('.swiper-slide-active');
+        let slider__lp1__active = lpSlider.querySelector('.swiper-slide-active');
+        // slider__lp1 = slider__lp1.nextElementSibling;
+    
+        let slider__lp2;
+        let slider__lp3;
+        let slider__lp0;
+    
+        if (slider__lp1.nextElementSibling) {
+            slider__lp2 = slider__lp1.nextElementSibling;
+        }
+    
+        if (slider__lp2 && slider__lp2.nextElementSibling) {
+            slider__lp3 = slider__lp2.nextElementSibling;
+        }
+    
+        if (slider__lp1.previousElementSibling) {
+            slider__lp0 = slider__lp1.previousElementSibling;
+        }
+    
+    
+        while (slider__lp0.previousElementSibling) {
+            slider__lp0.classList.add('smooth-hide');
+            slider__lp0.classList.remove('smooth-hide-half');
+            slider__lp0 = slider__lp0.previousElementSibling;
+        }
+    
+        while (slider__lp1.nextElementSibling) {
+            slider__lp1.nextElementSibling.classList.add('smooth-hide');
+            slider__lp1 = slider__lp1.nextElementSibling;
+        }
+
+        if (slider__lp2) {
+            slider__lp2.classList.remove('smooth-hide');
+            slider__lp2.classList.remove('smooth-hide-half');
+        }
+        if (slider__lp3) {
+            slider__lp3.classList.remove('smooth-hide');
+            slider__lp3.classList.add('smooth-hide-half');
+        }
+
+        slider__lp1__active.classList.remove('smooth-hide');
+        slider__lp1__active.classList.remove('smooth-hide-half');
+
+    }, 10);    
+}
+
 lpSlider.on('slideChange', function () {
-    let lpSlider = document.querySelector('.swiper.lp-slider');
-    let slider__lp1 = lpSlider.querySelector('.swiper-slide-active');
-    slider__lp1 = slider__lp1.nextElementSibling;
-    
-    let slider__lp2;
-    let slider__lp3;
-    let slider__lp0;
-
-    if (slider__lp1.nextElementSibling) {
-        slider__lp2 = slider__lp1.nextElementSibling;
-    }
-
-    if (slider__lp2 && slider__lp2.nextElementSibling) {
-        slider__lp3 = slider__lp2.nextElementSibling;
-    }
-    
-    if (slider__lp1.previousElementSibling) {
-        slider__lp0 = slider__lp1.previousElementSibling;
-    }
-
-
-    while (slider__lp0.previousElementSibling) {
-        slider__lp0.previousElementSibling.classList.add('smooth-hide');
-        slider__lp0 = slider__lp0.previousElementSibling;
-    }
-
-    while (slider__lp1.nextElementSibling) {
-        slider__lp1.nextElementSibling.classList.add('smooth-hide');
-        slider__lp1 = slider__lp1.nextElementSibling;
-    }
-
-
-    slider__lp1.classList.remove('smooth-hide');
-    if (slider__lp2) {
-        slider__lp2.classList.remove('smooth-hide');
-        slider__lp2.classList.remove('smooth-hide-half');
-    }
-    slider__lp1.classList.remove('smooth-hide-half');
-    if (slider__lp3) {
-        slider__lp3.classList.remove('smooth-hide');
-        slider__lp3.classList.add('smooth-hide-half');
-    }
-    
-
+    lpSwiperMagic();
 });
+
+lpSwiperMagic();
