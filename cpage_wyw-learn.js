477c807f37d85093a62a228b09104aafd0c85911
arbass <13ar.bass@gmail.com>
2022-10-20 19:40:42 +0300

    updated: wyw scripot

--- cpage_wyw-learn.js
+++ cpage_wyw-learn.js
@@ -22,52 +22,23 @@ if (currentSrc.textContent != '') {
         wywSubline.textContent = sitickyContent[1];
         wywSubline.classList.remove('hide');
     }
+    //находим все quote
+    let quotes = currentSrc.querySelectorAll('blockquote');
     
+    function renderBlock__1 () {
+        
+    }
+    
+    function renderBlock__2 () {
+        
+    }
+    
+    function renderBlock__3 () {
+        
+    }
 
-    //определяем список необходимых секций для правой колонки
-    let requiredSections__quote = currentSrc.querySelector('blockquote').textContent.split(',');
-
-    let allLearnCol = wywSection.querySelectorAll('[wyw-learn-right]');
-    allLearnCol.forEach(learnCol => {
-        learnCol.classList.add('hide');
-    });
-
-    requiredSections__quote.forEach(quoteNumber => {
-        wywSection.querySelector(`[wyw-learn-right="${quoteNumber}"]`).classList.remove('hide');
-
-        if (quoteNumber == '1') {
-            let title__1 = currentSrc.querySelectorAll('h3')[0].textContent;
-            console.log(title__1);
-
-            let contentWaiter =  wywSection.querySelector(`[wyw-learn-right="${quoteNumber}"] .w-richtext`);
-            let currentTitle =  wywSection.querySelector(`[wyw-learn-right-takeways="title"]`);
-            currentTitle.textContent = title__1;
-            let allInnereElements = contentWaiter.querySelectorAll('*');
-
-            allInnereElements.forEach(el => {
-                el.remove();
-            });
-
-            //тут нужно просто весь контент вставить, без изменений
-            let startPointerForReachTextParsing = currentSrc.querySelectorAll('h3')[0].nextElementSibling;
-            while (startPointerForReachTextParsing.nextElementSibling.tagName != 'H3') {
-                elAppender = startPointerForReachTextParsing.cloneNode(true);
-                contentWaiter.append(elAppender);
-                startPointerForReachTextParsing = startPointerForReachTextParsing.nextElementSibling;
-            }
-
-            
-        }
-
-        if (quoteNumber == '2') {
-            let title__2 = currentSrc.querySelectorAll('h3')[1].textContent;
-            
-        }
-
-        if (quoteNumber == '3') {
-            let title__3 = currentSrc.querySelectorAll('h3')[2].textContent;
-            
-        }
+    quotes.forEach(quotes__item => {
+        window['renderBlock__' + quotes__item.textContent]();
     });
 
 }
