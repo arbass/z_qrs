154d8bb8ef74e028649cf8425975d0ba064a7e6d
arbass <13ar.bass@gmail.com>
2022-10-21 21:25:39 +0300

    updated: roadmap.js

--- module-map.js
+++ module-map.js
@@ -1,17 +1,20 @@
-// let roadmap__section = document.querySelector('[cpage-section="roadmap"]');
-// let roadmap__src = roadmap__section.querySelector('.text-rich-text.is-src');
+let roadmap__section = document.querySelector('[cpage-section="roadmap"]');
+let roadmap__src = roadmap__section.querySelector('.text-rich-text.is-src');
 
-// let roadmap__labels = roadmap__section.querySelectorAll('.l-component-road_roadmap-item .is-roadmap-tag');
-// let roadmap__headlines = roadmap__section.querySelectorAll('.l-component-road_roadmap-item h3');
+let roadmap__labels = roadmap__section.querySelectorAll('.l-component-road_roadmap-item .is-roadmap-tag');
+let roadmap__headlines = roadmap__section.querySelectorAll('.l-component-road_roadmap-item h3');
 
-// let src__h5 = roadmap__src.querySelectorAll('h5');
-// let src__h6 = roadmap__src.querySelectorAll('h6');
+let roadmap__src__h5 = roadmap__src.querySelectorAll('h5');
+let roadmap__src__h6 = roadmap__src.querySelectorAll('h6');
 
+roadmap__src__h5.forEach((el, elId) => {
+    if (roadmap__labels[elId] != undefined) {
+        roadmap__labels[elId].textContent = el.textContent;
+    }
+});
 
-// src__h5.forEach((h5, h5__id) => {
-//     roadmap__labels[h5__id].textContent = h5.textContent;
-// });
-
-// src__h6.forEach((h6, h6__id) => {
-//     roadmap__headlines[h6__id].textContent = h6.textContent;
-// });
+roadmap__src__h6.forEach((el, elId) => {
+    if (roadmap__headlines[elId] != undefined) {
+        roadmap__headlines[elId].textContent = el.textContent;
+    }
+});
