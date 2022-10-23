8bd22acd1a912e9b7bfb27722803b62e760e1ad9
arbass <13ar.bass@gmail.com>
2022-10-23 20:07:00 +0300

    updated: cta.js

--- cta.js
+++ cta.js
@@ -1,10 +1,21 @@
 let cta__section = document.querySelector('[cpage-section="cta"]');
 let cta__src = cta__section.querySelector('.text-rich-text.is-src');
-let cta__headline = cta__section.querySelector('.is-cta_headline');
-let cta__subline = cta__section.querySelector('.cta_subline');
 
-let cta__headline_src = cta__section.querySelector('h2');
-let cta__subline_src = cta__section.querySelector('h3');
+let cta__headline = cta__section.querySelector("h2");
+let cta__subline = cta__section.querySelector("p");
+cta__headline.classList.add("hide");
+cta__subline.classList.add("hide");
 
-cta__headline.textContent = cta__headline_src.textContent;
-cta__subline.textContent = cta__subline_src.textContent;
+if (cta__src.textContent != '') {
+    let src__headings = cta__src.querySelectorAll("h2, h3");
+    src__headings.forEach(heading => {
+        if (heading.tagName == "H2") {
+            cta__headline.textContent = heading.textContent;
+            cta__headline.classList.remove("hide");
+        } 
+        if (heading.tagName == "H3") {
+            cta__subline.textContent = heading.textContent;
+            cta__subline.classList.remove("hide");
+        } 
+    });
+}
