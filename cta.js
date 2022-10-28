ac72760db2997f44a4a778410f7e6acce9cd9199
arbass <13ar.bass@gmail.com>
2022-10-28 16:02:39 +0300

    updated: cta.js

--- cta.js
+++ cta.js
@@ -1,8 +1,8 @@
 let cta__section = document.querySelector('[cpage-section="cta"]');
 let cta__src = cta__section.querySelector('.text-rich-text.is-src');
 
-let cta__headline = cta__section.querySelector("h2");
-let cta__subline = cta__section.querySelector("p");
+let cta__headline = cta__section.querySelector('[cta-el="title"]');
+let cta__subline = cta__section.querySelector('[cta-el="subtitle"]');
 cta__headline.classList.add("hide");
 cta__subline.classList.add("hide");
 
@@ -11,9 +11,11 @@ if (cta__src.textContent != '') {
     src__headings.forEach(heading => {
         if (heading.tagName == "H2") {
             cta__headline.textContent = heading.textContent;
+            console.log(heading.textContent);
             cta__headline.classList.remove("hide");
         } 
         if (heading.tagName == "H3") {
+            console.log(heading.textContent);
             cta__subline.textContent = heading.textContent;
             cta__subline.classList.remove("hide");
         } 
